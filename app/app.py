8d573e018160c14359e33f6cb7d5bf7eb29b3a85
AaryanKulkarni125 <aaryanyk@gmail.com>
2026-05-27 21:07:27 +0530

    Basic frontend done

--- app/app.py
+++ app/app.py
@@ -1,3 +1,18 @@
 import streamlit as st 
 
 st.title("Movie Recommendation System")
+
+from src.recommender import recommend
+from src.recommender import movies
+
+#select a movie from the dropdown (a movie you have watched)
+
+selected_movie=st.selectbox("select a movie", movies['title'].values)
+
+if st.button("Recommend"):
+    recommendations=recommend(selected_movie)
+
+    for movie in recommendations:
+        st.write(movie)
+
+        
